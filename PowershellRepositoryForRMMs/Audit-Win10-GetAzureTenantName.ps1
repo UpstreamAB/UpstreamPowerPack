a11ecca1ecd5f57eab01dbd275d537f90bd03b73
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-07 14:58:52 +0100

    Delete Audit-Win10-GetAzureTenantName.ps1

--- PowershellRepositoryForRMMs/Audit-Win10-GetAzureTenantName.ps1
+++
@@ -1,8 +0,0 @@
-$subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo"
-
-$guids = $subKey.GetSubKeyNames()
-foreach($guid in $guids) {
-    $guidSubKey = $subKey.OpenSubKey($guid);
-    $DisplayName = $guidSubKey.GetValue("DisplayName");
-}
-write-output $DisplayName
