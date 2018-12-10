eccd1f9278d1640220db470ab5ffab567efac8e7
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-10 13:04:53 +0100

    Update Audit-GetAzureTenantName.ps1

--- KaseyaVSA/Powershell/Audit-GetAzureTenantName.ps1
+++ KaseyaVSA/Powershell/Audit-GetAzureTenantName.ps1
@@ -1,3 +1,11 @@
+# Script name: RMM-AppMgmnt-Deploy-Windows-Webroot.
+# Script type: PowerShell.
+# Script description: Deploy Webroot silently with associated Webroot MSP site key.
+# Dependencies: Webroot site key to be added as $WebrootKey variable.
+# Supported OS: Windows Server 2012, Windows Server 2016, Windows Server 2019, Windows 7, Windows 10.
+# Script maintainer: powerpack@upstream.se
+# https://www.upstream.com/powerpack"
+
 $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo"
 
 $guids = $subKey.GetSubKeyNames()
