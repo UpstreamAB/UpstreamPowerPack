3dd0d53e9edada722927f4ae5efc7f9eb4b98caa
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-10 08:14:03 +0100

    Update RMM-AppMgmnt-Windows-Upgrade-All-With-Chocolatey-Framework.bat

--- RMM-PowerShell-Repository/AppMgmnt/RMM-AppMgmnt-Windows-Upgrade-All-With-Chocolatey-Framework.bat
+++ RMM-PowerShell-Repository/AppMgmnt/RMM-AppMgmnt-Windows-Upgrade-All-With-Chocolatey-Framework.bat
@@ -1,11 +1,11 @@
-REM Script name: RMM-AppMgmnt-Windows-Upgrade-All-With-Chocolatey-Framework.bat
-REM Script type: Batch.
-REM Script description: Upgrade all supported applications with the Chocolatey application management framework.
-REM Dependencies: Powershell.
-REM Supported OS: Windows Server 2012, Windows Server 2016, Windows Server 2019, Windows 7, Windows 10.
-REM Script maintainer: powerpack@upstream.se
-REM https://www.upstream.com/powerpack"
-REM
+:: Script name: RMM-AppMgmnt-Windows-Upgrade-All-With-Chocolatey-Framework.bat
+:: Script type: Batch.
+:: Script description: Upgrade all supported applications with the Chocolatey application management framework.
+:: Dependencies: Powershell.
+:: Supported OS: Windows Server 2012, Windows Server 2016, Windows Server 2019, Windows 7, Windows 10.
+:: Script maintainer: powerpack@upstream.se
+:: https://www.upstream.com/powerpack"
+::
 @ECHO UPSTREAM: Upgrade All With Chocolatey: Upgrading all supported apps.
 c:\ProgramData\Chocolatey\choco.exe upgrade all --limit-output --no-progress -y
-@ECHO UPSTREAM: Upgrade All With Chocolatey: End of script: Evaluate the script output."     
+@ECHO UPSTREAM: Upgrade All With Chocolatey: End of script: Evaluate the script output."
