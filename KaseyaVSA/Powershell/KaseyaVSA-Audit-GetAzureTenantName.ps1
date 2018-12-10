9cb3a296c98cdab08de1bcb51c02aca6153014eb
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-10 15:22:17 +0100

    Update KaseyaVSA-Audit-GetAzureTenantName.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureTenantName.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureTenantName.ps1
@@ -13,5 +13,5 @@ foreach($guid in $guids) {
     $guidSubKey = $subKey.OpenSubKey($guid);
     $DisplayName = $guidSubKey.GetValue("DisplayName");
 }
-#Let's write the Azure Tenant name to the concole for Kaseya VSA to pick up as a variable.
+# Let's write the Azure Tenant name to the concole for Kaseya VSA to pick up as a variable.
 write-output $DisplayName
