8b0163d3cbac8576989c192ae8d065bbf6cd1e6f
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-12 13:25:07 +0100

    Create KaseyaVSA-Audit-GetAzureUserName.ps1

---
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureUserName.ps1
@@ -0,0 +1,8 @@
+$subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+
+$guids = $subKey.GetSubKeyNames()
+foreach($guid in $guids) {
+    $guidSubKey = $subKey.OpenSubKey($guid);
+    $UserEmail = $guidSubKey.GetValue("UserEmail");
+}
+write-output $UserEmail
