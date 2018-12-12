28f44edc1ea65ae877aedb29e261f91800960802
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-12 13:28:12 +0100

    Update KaseyaVSA-Audit-GetAzureUserName.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureUserName.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureUserName.ps1
@@ -1,3 +1,11 @@
+# Script name: KaseyaVSA-Audit-GetAzureUserName.ps1
+# Script type: PowerShell.
+# Script description: Audit the Azure tenant user name on a Windows 10 computer.
+# Dependencies: Azure AD joined.
+# Supported OS: Windows 10.
+# Script maintainer: powerpack@upstream.se
+# https://www.upstream.com/powerpack"
+
 $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
 
 $guids = $subKey.GetSubKeyNames()
