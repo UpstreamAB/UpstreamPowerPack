4ca105bf612ab7cbf6e9545923f29f063597e00e
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-20 12:48:55 +0100

    Update RMM-PatchMgmnt-Windows10-SetActiveHours.ps1

--- RMM-Script-Repository/PatchMgmnt/RMM-PatchMgmnt-Windows10-SetActiveHours.ps1
+++ RMM-Script-Repository/PatchMgmnt/RMM-PatchMgmnt-Windows10-SetActiveHours.ps1
@@ -5,7 +5,8 @@
 # Script maintainer: powerpack@upstream.se
 # https://www.upstream.com/powerpack
 
-Write-host "UPSTREAM: Set Active Hours: Setting Active Hours to 06:00-22:00 in the registry.
-Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursStart -Value 6 -PassThru
-Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursEnd -Value 22 -PassThru
-Write-host "UPSTREAM: Deploy Webroot: End of PowerShell script: Evaluate the console output.
+Write-host "UPSTREAM: Script name: RMM-PatchMgmnt-Windows10-SetActiveHours.ps1"
+Write-host "UPSTREAM: Setting Active Hours to 06:00-22:00 in the registry."
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name ActiveHoursStart -Value 7 -PassThru
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name ActiveHoursEnd -Value 18 -PassThru
+Write-host "UPSTREAM: Deploy Webroot: End of PowerShell script: Evaluate the console output."
