6620a2536cebec697f26487bf21be0a5fcc80db4
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2018-12-21 11:40:12 +0100

    Update and rename RMM-SysMgmnt-Win10-SetActiveHours.bat to RMM-PatchMgmnt-Windows10-SetActiveHours.ps1

--- RMM-Script-Repository/SysMgmnt/RMM-SysMgmnt-Win10-SetActiveHours.bat
+++
@@ -1,6 +0,0 @@
-REM ****************
-REM Enable Active Hours
-REM ****************
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v fActiveHoursEnabled /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v fActiveHoursStart /t REG_DWORD /d 6 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v fActiveHoursEnd /t REG_DWORD /d 22 /f
