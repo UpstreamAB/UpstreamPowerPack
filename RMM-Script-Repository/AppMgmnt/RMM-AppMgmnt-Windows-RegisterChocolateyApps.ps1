601bf34a89113a49ad6ba093b2b2dbd4cb434b9c
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-08 13:04:17 +0100

    Update RMM-AppMgmnt-Windows-RegisterChocolateyApps.ps1

--- RMM-Script-Repository/AppMgmnt/RMM-AppMgmnt-Windows-RegisterChocolateyApps.ps1
+++ RMM-Script-Repository/AppMgmnt/RMM-AppMgmnt-Windows-RegisterChocolateyApps.ps1
@@ -1,34 +1,33 @@
 # Script name: RMM-AppMgmnt-Windows-RegisterChocolateyApps.ps1
 # Script type: Powershell
-# Script description: Register local list of applications to be managed by the Chocolatey
+# Script description: Register local list of applications to be managed by the Chocolatey by re-deploying.
 # Dependencies: Powershell 3.0
 # Supported OS: Windows Server 2012, Windows Server 2016, Windows Server 2019, Windows 7, Windows 10
 # Script maintainer: powerpack@upstream.se
 # https://www.upstream.com/powerpack"
 # ------------------------------------------------------------------------------------------------------------------------------
 
+# Step 1. Check for dependecies.
 #Requires -Version 3
-
-# Check if Chocolatey is installed on local machine. If not, let's install.
+# Let's check if Chocolatey is installed on local machine. If not, install and continue.
 if (Test-Path "C:\ProgramData\Chocolatey\choco.exe")
 {
 	Write-Output "UPSTREAM: Great! Chocolatey is already installed on this machine. Let's continue."
 }
 else
 {
-	Write-Output "UPSTREAM: Whoops! Chocolatey not installed on this machine. Let's install and continue."
+	Write-Output "UPSTREAM: Whoops! Chocolatey is not installed on this machine. Let's install and continue."
 	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+# Step 2. Create some variables.
 # Let's get applications already managed with Chocolatey by getting a list and feed it to a varible.
 $AppsCurrentlyManagedByChocolatey = C:\ProgramData\Chocolatey\choco.exe list --local-only
-
 # Let's get installed applications from Add/Remove Programs to a variable.
 # 32Bit Windows apps.
 $InstalledAppsFromAddRemove = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Select-Object -ExpandProperty DisplayName
 # 64Bit Windows apps.
 $InstalledAppsFromAddRemove += Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Select-Object -ExpandProperty DisplayName
-
 # Let's create a list of packages we want to be managed with Chocolatey. Packages can be viewed here: https://chocolatey.org/packages.
 $ChocolateyPackageName = @(
 	"flashplayerppapi"
@@ -43,21 +42,20 @@ $LocalRegistryAppName = @(
 	"Mozilla Firefox"
 	"Google Chrome"
 )
-
+# Step 3. Make some needed preparations with running web browsers.
 # If Adobe Flash Player 32 NPAPI is installed on local machine but not yet managed with Chocolatey, we need to close Chrome in order to install. This is a one time thing.
 if ($LocalRegistryAppName["Mozilla Firefox"] -NotMatch $ChocolateyPackageName["flashplayerplugin"])
 {
 	Write-Output "UPSTREAM: Adobe Flash Player 32 NPAPI detected on local machine. In order to install properly with Choholatey we need to close Firefox during installation."
 	Stop-Process -processname "firefox"
 }
-
 # If Adobe Flash Player 32 PPAPI is installed on local machine but not yet managed with Chocolatey, we need to close Chrome in order to install. This is a one time thing.
 if ($LocalRegistryAppName["Google Chrome"] -NotMatch $ChocolateyPackageName["flashplayerppapi"])
 {
 	Write-Output "UPSTREAM: Adobe Flash Player 32 PPAPI detected on local machine. In order to install properly with Choholatey we need to close Chrome during installation."
 	Stop-Process -processname "chrome"
 }
-
+#Step 4: Compare list of local apps against aproved Chocolatey apps.
 # If any local Add/Remove app from $LocalRegistryAppName above are missing in $ChocolateyPackageName we will re-deploy the app with Chocolatey in order for it to register.
 for ($index = 0; $index -lt $ChocolateyPackageName.length; $index++)
 {
