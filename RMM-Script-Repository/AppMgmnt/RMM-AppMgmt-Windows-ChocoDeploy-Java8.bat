b31f6ca70748e4c3216f744f96385445dbbcfc69
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-09 11:24:35 +0100

    Update RMM-AppMgmt-Windows-ChocoDeploy-Java8.bat

--- RMM-Script-Repository/AppMgmnt/RMM-AppMgmt-Windows-ChocoDeploy-Java8.bat
+++ RMM-Script-Repository/AppMgmnt/RMM-AppMgmt-Windows-ChocoDeploy-Java8.bat
@@ -1,8 +1,7 @@
 :: Script name: RMM-AppMgmnt-Windows-ChocoDeploy-Java8.bat
 :: Script type: Batch.
 :: Script description: Deploy latest Java 8 with the Chocolatey application management framework.
-:: Dependencies: Powershell.
-:: Supported OS: Windows Server 2012, Windows Server 2016, Windows Server 2019, Windows 7, Windows 10.
+:: Dependencies: Chocolatey.
 :: Script maintainer: powerpack@upstream.se
 :: https://www.upstream.com/powerpack
 ::
