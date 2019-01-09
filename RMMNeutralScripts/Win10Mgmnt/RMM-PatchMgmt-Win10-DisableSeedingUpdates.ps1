d41f77feda7d2858e43ee68b59dbe865bf34f4c1
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-09 16:37:16 +0100

    Update RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1

--- RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
+++ RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
@@ -1,1 +1,8 @@
+# Script name: RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
+# Script type: PowerShell
+# Script description: Disbles the seeding feature in Windows 10 to share downloaded patches with machines nearby.
+# Dependencies: Windows 10
+# Script maintainer: powerpack@upstream.se
+# https://www.upstream.com/powerpackWrite-Output 
+
 Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name DODownloadMode -Value 0 -PassThru
