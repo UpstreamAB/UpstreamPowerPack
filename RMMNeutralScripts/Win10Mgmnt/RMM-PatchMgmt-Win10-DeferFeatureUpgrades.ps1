c41bf9061229348c0571c9d5dfb8e098da52eff5
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 17:01:52 +0100

    Update RMM-PatchMgmt-Win10-DeferFeatureUpgrades.ps1

--- RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmt-Win10-DeferFeatureUpgrades.ps1
+++ RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmt-Win10-DeferFeatureUpgrades.ps1
@@ -3,9 +3,7 @@
 # Script description: Setting a 365 days waiting period for any new Windows 10 release (like for example Windows 10 October release 2018)
 # Dependencies: Windows 10
 # Script maintainer: powerpack@upstream.se
-# https://www.upstream.com/powerpack
+# https://en.upstream.se/powerpack
 
-Write-Output "UPSTREAM: Executing Powershell script RMM-PatchMgmnt-Windows10-DeferFeatureUpgrades.ps1"
 Write-Output "UPSTREAM: Setting a 365 days waiting period for any new Windows 10 version upgrade."
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name DeferFeatureUpdatesPeriodInDays -Value 365 -PassThru
-Write-Output "UPSTREAM: End of PowerShell script: Evaluate the console output."
