58c29114f50b6eef776844b090d428a4e94cce99
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 16:11:43 +0100

    Update KaseyaVSA-Audit-GetCurrentFreeDisk.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
@@ -8,5 +8,5 @@
 
 $DiskInfo = get-wmiobject -class "Win32_LogicalDisk" -namespace "root\CIMV2" | Where-Object DeviceID -Like C:
 $DiskFree = [math]::round($DiskInfo.FreeSpace/1GB, 0)
-# Let's write the current free disk infomation to the concole for Kaseya VSA to pick up as a variable.
+# Let's write the current free disk in GB for Kaseya VSA to pick up as a variable.
 Write-Output $DiskFree
