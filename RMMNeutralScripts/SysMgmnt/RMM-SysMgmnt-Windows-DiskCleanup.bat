272f521b10b6147648bd31afa38413f452458bab
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 12:54:39 +0100

    Update RMM-SysMgmnt-Windows-DiskCleanup.bat

--- RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
+++ RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
@@ -1,11 +1,28 @@
 # Script name: RMM-AppMgmt-Windows-DiskCleanup.ps1
 # Script type: Powershell
-# Script description: Installs 7zip on local machine.
+# Script description: .
 # Dependencies: Powershell 3.0
 # Script maintainer: powerpack@upstream.se
 # https://en.upstream.se/powerpack/
 # --------------------------------------------------------------------------------------------------------------------------------
 
-#Requires -Version 3
+$CurrentFreeDiskInfo = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
+| Format-Table DeviceID, MediaType, `
+			   @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
+			   @{ Name = "Free Space(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.freespace/1gb)) } }, `
+			   @{ Name = "Free (%)"; Expression = { "{0,6:P0}" -f (($_.freespace/1gb) / ($_.size/1gb)) } } `
+			   -AutoSize
 
-cleanmgr.exe /verylowdisk /d c
+Write-Output "UPSTREAM: This is the disk space before cleanup: "$CurrentFreeDiskInfo
+
+# Let's call Windows internal diskcleaner and let it it's thing.
+Start-Process c:\windows\system32\cleanmgr.exe -ArgumentList "/verylowdisk /d c" -NoNewWindow -Wait
+
+$CurrentFreeDiskInfo = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
+| Format-Table DeviceID, MediaType, `
+			   @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
+			   @{ Name = "Free Space(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.freespace/1gb)) } }, `
+			   @{ Name = "Free (%)"; Expression = { "{0,6:P0}" -f (($_.freespace/1gb) / ($_.size/1gb)) } } `
+			   -AutoSize
+
+Write-Output "UPSTREAM: This is the disk space after cleanup: "$CurrentFreeDiskInfo
