f663bbb2e6d109c9193e064aa1cc757e6da866d8
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 11:38:47 +0100

    Create RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1

---
+++ RMMNeutralScripts/AppMgmtWithChocolatey/RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1
@@ -0,0 +1,22 @@
+# Script name: RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1
+# Script type: Powershell
+# Script description: Installs Adobe Flash Player 32 PPAPI for Google Chrome on local machine.
+# Dependencies: Powershell 3.0
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
+
+#Requires -Version 3
+# Let's check if Chocolatey is installed on local machine. If not, install.
+Write-Output "UPSTREAM: Checking for Chocolatey on this machine."
+if (Test-Path "C:\ProgramData\Chocolatey\choco.exe")
+{
+	Write-Output "UPSTREAM: Great! Chocolatey is already installed on this machine. Let's go!"
+}
+else
+{
+	Write-Output "UPSTREAM: Whoops! Chocolatey is missing on this machine. Let's install and carry on."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+# This line will tell Chocolatey to install the Adobe Flash Player 32 PPAPI package.
+c:\ProgramData\Chocolatey\choco.exe install flashplayerppapi --limit-output --no-progress -y
