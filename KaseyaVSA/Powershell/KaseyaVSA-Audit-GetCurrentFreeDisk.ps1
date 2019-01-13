7c6998418232dfc11c290c9be4837dee878ec618
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 15:36:13 +0100

    Update KaseyaVSA-Audit-GetCurrentFreeDisk.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
@@ -10,5 +10,6 @@ $CurrentFreeDisk = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
 | Format-Table @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
 			   @{ Name = "Free Space(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.freespace/1gb)) } }, `
 			   @{ Name = "Free (%)"; Expression = { "{0,6:P0}" -f (($_.freespace/1gb) / ($_.size/1gb)) } } `
+			   -AutoSize
 # Let's write the current free disk infomation to the concole for Kaseya VSA to pick up as a variable.
 Write-Output $CurrentFreeDisk
