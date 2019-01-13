d8114ab57f4f7020538647f8a16889636d08739b
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 16:09:27 +0100

    Update KaseyaVSA-Audit-GetCurrentFreeDisk.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
@@ -1,15 +1,12 @@
 # Script name: KaseyaVSA-Audit-GetCurrentFreeDisk.ps1
-# Related Kaseya Agent Procedure: "Audit - Windows 10 - Custom Field - Azure AD Info"
+# Related Kaseya Agent Procedure: "Audit - Windows - Get Current Free Disk"
 # Script description: Audit the Azure tenant name on a Windows 10 computer
 # Dependencies: Existing registry values
 # Supported OS: Windows
 # Script maintainer: powerpack@upstream.se
 # https://en.upstream.se/powerpack
 
-$CurrentFreeDisk = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
-| Format-Table @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
-			   @{ Name = "Free Space(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.freespace/1gb)) } }, `
-			   @{ Name = "Free (%)"; Expression = { "{0,6:P0}" -f (($_.freespace/1gb) / ($_.size/1gb)) } } `
-			   -AutoSize
+$DiskInfo = get-wmiobject -class "Win32_LogicalDisk" -namespace "root\CIMV2" | Where-Object DeviceID -Like C:
+$DiskFree = [math]::round($DiskInfo.FreeSpace/1GB, 0)
 # Let's write the current free disk infomation to the concole for Kaseya VSA to pick up as a variable.
-Write-Output $CurrentFreeDisk
+Write-Output "UPSTREAM: Free disk in GB: "$DiskFree
