dd8ec68a3f17ace18dede7339ac4b968af508b43
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 12:15:58 +0100

    Update RMM-SysMgmnt-Windows-DiskCleanup.bat

--- RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
+++ RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
@@ -1,1 +1,11 @@
+# Script name: RMM-AppMgmt-Windows-DiskCleanup.ps1
+# Script type: Powershell
+# Script description: Installs 7zip on local machine.
+# Dependencies: Powershell 3.0
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
+
+#Requires -Version 3
+
 cleanmgr.exe /verylowdisk /d c
