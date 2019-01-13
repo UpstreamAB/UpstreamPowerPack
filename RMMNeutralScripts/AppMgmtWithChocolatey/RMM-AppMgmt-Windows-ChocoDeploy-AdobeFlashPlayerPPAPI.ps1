c310818f30ca63bcf92109fe0d57d939e8684452
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 11:40:33 +0100

    Update RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1

--- RMMNeutralScripts/AppMgmtWithChocolatey/RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1
+++ RMMNeutralScripts/AppMgmtWithChocolatey/RMM-AppMgmt-Windows-ChocoDeploy-AdobeFlashPlayerPPAPI.ps1
@@ -18,5 +18,7 @@ else
 	Write-Output "UPSTREAM: Whoops! Chocolatey is missing on this machine. Let's install and carry on."
 	Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
+Write-Output "UPSTREAM: In order to install properly with Choholatey we need to close Chrome during installation if running."
+Get-Process | Where-Object { $_.Name -eq "chrome" } | Select-Object -First 1 | Stop-Process
 # This line will tell Chocolatey to install the Adobe Flash Player 32 PPAPI package.
 c:\ProgramData\Chocolatey\choco.exe install flashplayerppapi --limit-output --no-progress -y
