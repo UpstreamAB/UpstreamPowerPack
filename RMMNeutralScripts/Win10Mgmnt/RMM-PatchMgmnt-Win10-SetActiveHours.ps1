9095bbe2dd085603bad428dee6b936b5ce104a6b
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-13 17:02:58 +0100

    Update RMM-PatchMgmnt-Win10-SetActiveHours.ps1

--- RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmnt-Win10-SetActiveHours.ps1
+++ RMMNeutralScripts/Win10Mgmnt/RMM-PatchMgmnt-Win10-SetActiveHours.ps1
@@ -5,7 +5,6 @@
 # Script maintainer: powerpack@upstream.se
 # https://www.upstream.com/powerpack
 
-Write-host "UPSTREAM: Script name: RMM-PatchMgmnt-Windows10-SetActiveHours.ps1"
 Write-host "UPSTREAM: Setting Active Hours to 06:00-22:00 in the registry."
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name ActiveHoursStart -Value 6 -PassThru
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name ActiveHoursEnd -Value 20 -PassThru
