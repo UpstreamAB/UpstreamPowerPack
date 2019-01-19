e742a071248a4539137070e4294fd28ead350c48
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-19 08:19:13 +0100

    Update RMM-Audit-Win10-GetAzureTenantName.ps1

--- RMMNeutralScripts/Win10Mgmnt/RMM-Audit-Win10-GetAzureTenantName.ps1
+++ RMMNeutralScripts/Win10Mgmnt/RMM-Audit-Win10-GetAzureTenantName.ps1
@@ -1,7 +1,11 @@
-RMM-Audit-Win10-GetAzureTenantName.ps1
+# Script name: RMM-Audit-Win10-GetAzureTenantName.ps1
+# Script type: PowerShell
+# Script description: Audits the Windows 10 machine for Azure AD tenant name.
+# Dependencies: Windows 10
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack
 
 $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo"
-
 $guids = $subKey.GetSubKeyNames()
 foreach($guid in $guids) {
     $guidSubKey = $subKey.OpenSubKey($guid);
