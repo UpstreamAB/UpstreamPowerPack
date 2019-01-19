7eb63d9f2a7cf2b31aeb987771d175fd6febdd5e
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-19 08:59:06 +0100

    Update RMM-SysMgmnt-Windows-DiskCleanup.bat

--- RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
+++ RMMNeutralScripts/SysMgmnt/RMM-SysMgmnt-Windows-DiskCleanup.bat
@@ -6,7 +6,6 @@
 # https://en.upstream.se/powerpack/
 # --------------------------------------------------------------------------------------------------------------------------------
 
-# First, let's have a look at current disk space free.
 $CurrentFreeDiskInfo = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
 | Format-Table DeviceID, MediaType, `
 			   @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
@@ -16,11 +15,9 @@ $CurrentFreeDiskInfo = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
 
 Write-Output "UPSTREAM: Disk space before cleanup: "$CurrentFreeDiskInfo
 
-# Let's call Windows internal diskcleaner and let it it's thing.
-Write-Output "UPSTREAM: Attemtig to do some disk cleanup with Cleanmgr. Wait for it...
+Write-Output "UPSTREAM: Let's call for cleanmgr.exe and let it do the work. Wait for it..."
 Start-Process -FilePath Cleanmgr -ArgumentList '/autoclean' -Wait
 
-# Now, let's figure our if Cleanmgr did it's job.
 $CurrentFreeDiskInfo = Get-WmiObject Win32_logicaldisk -ComputerName LocalHost `
 | Format-Table DeviceID, MediaType, `
 			   @{ Name = "Size(GB)"; Expression = { [decimal]("{0:N0}" -f ($_.size/1gb)) } }, `
