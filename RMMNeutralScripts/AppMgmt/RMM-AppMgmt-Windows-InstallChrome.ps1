a0c1daa3fb2205a371305daf6537065b9e9a8ff3
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-19 20:58:12 +0100

    Create RMM-AppMgmt-Windows-InstallChrome.ps1

---
+++ RMMNeutralScripts/AppMgmt/RMM-AppMgmt-Windows-InstallChrome.ps1
@@ -0,0 +1,43 @@
+<#	
+	.NOTES
+	===========================================================================
+		Filename:     	RMM-AppMgmt-Windows-InstallChrome.ps1
+		Latest update:  2019-01-19
+		Created by:   	powerpack@upstream.se
+		Organization: 	Upstream AB, https://en.upstream.se/powerpack
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+# What file do you want to download? Enter the path here.
+$Url = "https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi"
+# Where do you want to put the file on the local drive?
+$File = "c:\temp\googlechromestandaloneenterprise64.msi"
+# What is the application name? Use the name that will be presented in the Add/Remove Control Panel.
+$AppName = "Google Chrome"
+# The method for downloading the file. This you do not want to change.
+$WebClient = New-Object System.Net.WebClient
+
+Write-Output "UPSTREAM: Downloading installer from" $Url "To local directory:" $File "Waiting for completion."
+$WebClient.DownloadFile($Url, $File)
+
+# Deploy the software.
+Write-Output "UPSTREAM: Installation begins. Waiting for completion."
+Start-Process $File /qn -Wait
+
+# Remove the installer after completion.
+#Remove-Item -Path $File -Force
+
+# Validate successful installation by looking for $AppName in the Add/Remove Programs list. 
+$IsAppInstalledOrNot = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -Match "$AppName" }
+
+if ($IsAppInstalledOrNot -Match $AppName)
+{
+	Write-Output $IsAppInstalledOrNot
+	Write-Output "UPSTREAM: Application successfully installed."
+}
+else
+{
+	Write-Output "UPSTREAM: Application failed to install. We can't find $AppName in the Add/Remove Programs list."
+}
