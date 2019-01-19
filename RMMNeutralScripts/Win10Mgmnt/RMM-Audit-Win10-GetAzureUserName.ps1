38d72de07e2465debb198d165a73f3c84a9a2d43
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-19 08:20:20 +0100

    Update RMM-Audit-Win10-GetAzureUserName.ps1

--- RMMNeutralScripts/Win10Mgmnt/RMM-Audit-Win10-GetAzureUserName.ps1
+++ RMMNeutralScripts/Win10Mgmnt/RMM-Audit-Win10-GetAzureUserName.ps1
@@ -1,7 +1,11 @@
-RMM-Audit-Win10-GetAzureUserName.ps1
+# Script name: RMM-Audit-Win10-GetAzureUserName.ps1
+# Script type: PowerShell
+# Script description: Audits the Windows 10 machine for Azure AD user name.
+# Dependencies: Windows 10
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack
 
 $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
-
 $guids = $subKey.GetSubKeyNames()
 foreach($guid in $guids) {
     $guidSubKey = $subKey.OpenSubKey($guid);
