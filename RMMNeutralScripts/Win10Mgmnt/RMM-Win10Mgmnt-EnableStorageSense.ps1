77d1afda1cf5eb985dd0924119e1077ae917b33a
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-20 16:43:31 +0100

    Create RMM-Win10Mgmnt-EnableStorageSense.ps1

---
+++ RMMNeutralScripts/Win10Mgmnt/RMM-Win10Mgmnt-EnableStorageSense.ps1
@@ -0,0 +1,15 @@
+
+<#	
+	===========================================================================
+		Filename:     	RMM-Win10Mgmt-EnableStorageSense.ps1
+		Latest update:  2019-01-19
+		Created by:   	powerpack@upstream.se
+		Organization: 	Upstream AB, https://en.upstream.se/powerpack
+	===========================================================================
+	.DESCRIPTION
+		Uses the System.Net.WebClient function fo grab the Google Chrome MSI package from URL and install
+		silently with msiexec command. Reports back success or failure based on Add/Remove Programs list
+	
+	.CHANGELOG
+		First release.
+#>
