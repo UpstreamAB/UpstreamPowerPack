e0d97c76e7ec17097ea0c408ad9bd1be592fa797
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-01-20 07:29:28 +0100

    Update RMM-Audit-Windows-GetWifiNames.ps1

--- RMMNeutralScripts/Audit/RMM-Audit-Windows-GetWifiNames.ps1
+++ RMMNeutralScripts/Audit/RMM-Audit-Windows-GetWifiNames.ps1
@@ -1,2 +1,17 @@
-#This one-liner will get the list of Wifi names from a Windows machine.
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{[PSCustomObject]@{ Detected_Wifi_Names=$name }} | Format-Table -AutoSize 
+<#	
+	===========================================================================
+		Filename:     	RMM-Audit-Windows-GetWifiNames.ps1
+		Latest update:  2019-01-19
+		Created by:   	powerpack@upstream.se
+		Organization: 	Upstream AB, https://en.upstream.se/powerpack
+	===========================================================================
+	.DESCRIPTION
+		Uses the System.Net.WebClient function fo grab the Google Chrome MSI package from URL and install
+		silently with msiexec command. Reports back success or failure based on Add/Remove Programs list
+	
+  .NOTES
+		First release.
+#>
+
+$WifiNames = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{[PSCustomObject]@{ Detected_Wifi_Names=$name }} | Format-Table -AutoSize 
+Write-Output "UPSTREAM: $WifiNames
