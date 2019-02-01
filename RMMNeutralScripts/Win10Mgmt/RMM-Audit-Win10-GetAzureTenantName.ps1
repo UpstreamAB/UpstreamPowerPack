a9081a4d4697ea85ff272b48502d42dfd3750f64
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-02-01 14:07:19 +0100

    Update RMM-Audit-Win10-GetAzureTenantName.ps1

--- RMMNeutralScripts/Win10Mgmt/RMM-Audit-Win10-GetAzureTenantName.ps1
+++ RMMNeutralScripts/Win10Mgmt/RMM-Audit-Win10-GetAzureTenantName.ps1
@@ -1,9 +1,10 @@
 # Script name: RMM-Audit-Win10-GetAzureTenantName.ps1
-# Script type: PowerShell
-# Script description: Audits the Windows 10 machine for Azure AD tenant name.
-# Dependencies: Windows 10
+# Script type: Powershell
+# Script description: Uninstalls Google Chrome on local machine.
+# Dependencies: Powershell 3.0, Windows 10
 # Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
 
 $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo"
 $guids = $subKey.GetSubKeyNames()
