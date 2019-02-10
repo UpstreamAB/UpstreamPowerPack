d41cf66747bad35a156b12d6c84e676d6426907a
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-02-10 09:02:59 +0100

    Rename KaseyaVSA-Audit-GetHyperVHostName.ps1 to KaseyaVSA-Audit-GetHyperVHostName2.ps1

---
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetHyperVHostName2.ps1
@@ -0,0 +1,11 @@
+# Script name: KaseyaVSA-Audit-GetAzureUserName.ps1
+# Related Kaseya Agent Procedure: "Audit - Windows 10 - Custom Field - Azure AD Info"
+# Script description: Audit the Azure tenant user name on a Windows 10 computer
+# Dependencies: Existing registry values
+# Supported OS: Windows 10.
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack"
+
+$HyperVHostName = (get-item "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters").GetValue("HostName")
+# Let's write the Azure Tenant User Name to the concole for Kaseya VSA to pick up as a variable.
+Write-Output $HyperVHostName
