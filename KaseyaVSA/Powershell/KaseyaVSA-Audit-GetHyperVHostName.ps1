9186219a95f5e3e5614fa541c1ef6ef67fe2e397
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-02-19 10:30:08 +0100

    Delete KaseyaVSA-Audit-GetHyperVHostName.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetHyperVHostName.ps1
+++
@@ -1,8 +0,0 @@
-# Script name: KaseyaVSA-Audit-GetHyperVHostName.ps1
-# Related Kaseya Agent Procedure: 
-# Script description: 
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack
-
-$HyperVHostName = Get-VMhost | Select-Object Name
-write-output $HyperVHostName
