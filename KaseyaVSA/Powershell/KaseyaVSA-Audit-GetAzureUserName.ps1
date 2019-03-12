0cc63bfbfd22b3d33787a0bc42c98a3b9763bb01
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-03-12 13:51:13 +0100

    Update KaseyaVSA-Audit-GetAzureUserName.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureUserName.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureUserName.ps1
@@ -1,6 +1,7 @@
 # Script name: KaseyaVSA-Audit-GetAzureUserName.ps1
 # Related Kaseya Agent Procedure: "Audit - Windows 10 - Custom Field - Azure AD Info"
-# Script description: Audit the Azure tenant user name on a Windows 10 computer
+# Script description: Audit the Azure tenant user name on a Windows 10 computer.
+# Upload this Powershell script to your Kaseya Agent Procedures folder "VSASharedFiles\UpstreamPowerPack\Powershell".
 # Dependencies: Existing registry values
 # Supported OS: Windows 10.
 # Script maintainer: powerpack@upstream.se
