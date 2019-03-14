7cb17aa455a5ec34a3a358c1588368abdf495766
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-03-14 19:03:07 +0100

    Create RMM-SysMgmt-Windows-DiskHealth.ps1

---
+++ RMMNeutralScripts/SysMgmt/RMM-SysMgmt-Windows-DiskHealth.ps1
@@ -0,0 +1,7 @@
+# Script name: RMM-SysMgmt-Windows-DiskHealth.ps1
+# Script type: Powershell
+# Script description: .
+# Dependencies: Powershell 3.0
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
