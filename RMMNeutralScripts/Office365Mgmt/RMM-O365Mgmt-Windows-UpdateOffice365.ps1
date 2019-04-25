7367df0dd6a226bf273c51a9e332caa9b1e13a36
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-04-25 14:43:02 +0200

    Create RMM-O365Mgmt-Windows-UpdateOffice365.ps1

---
+++ RMMNeutralScripts/Office365Mgmt/RMM-O365Mgmt-Windows-UpdateOffice365.ps1
@@ -0,0 +1,11 @@
+# Script name: RMM-O365Mgmt-Windows-UpdateOffice365.ps1
+# Script type: Powershell
+# Script description: Updadates existign Office 365 installation with the latest patches.
+# Dependencies: Powershell 3.0
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
+
+$OfficeC2RClient = "%ProgramFiles%\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe"
+$Arguments = "/update user updatepromptuser=true forceappshutdown=true displaylevel=true"
+Start-Process $OfficeC2RClient $Arguments
