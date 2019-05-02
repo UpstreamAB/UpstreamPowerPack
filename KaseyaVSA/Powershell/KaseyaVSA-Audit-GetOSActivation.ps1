df8bd951298c4ccdfab781f7d60a1a53937aaf8a
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-05-02 12:15:05 +0200

    Update KaseyaVSA-Audit-GetOSActivation.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetOSActivation.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetOSActivation.ps1
@@ -1,7 +1,7 @@
 # Script name: KaseyaVSA-Audit-GetOSActivation.ps1
 # Related Kaseya Agent Procedure: "Audit - Windows - Is Windows Activated"
 # Script description: Audit if the Windows OS is activated with a valid license.
-# Upload this Powershell script to your Kaseya Agent Procedures folder "VSASharedFiles\UpstreamPowerPack\Powershell".
+# Upload this Powershell script to your Kaseya Agent Procedures, Manged Files folder "VSASharedFiles\UpstreamPowerPack\Powershell".
 # Supported OS: Windows OS.
 # Script maintainer: powerpack@upstream.se
 # https://en.upstream.se/powerpack
