463538c7e3b818302600ed6c73b4a5e9aa8d7600
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-05-10 10:12:09 +0200

    Update RMM-Audit-O365Mgmt-Windows-CheckOffice365License.ps1

--- RMMNeutralScripts/Office365Mgmt/RMM-Audit-O365Mgmt-Windows-CheckOffice365License.ps1
+++ RMMNeutralScripts/Office365Mgmt/RMM-Audit-O365Mgmt-Windows-CheckOffice365License.ps1
@@ -6,34 +6,40 @@
 # https://en.upstream.se/powerpack/
 # --------------------------------------------------------------------------------------------------------------------------------
  
-# First, let's look for Office 365 on the local machine. Perticularly the OSPP.VBS file we are going to use in this script.
+# First, let's look for Office 365 on the local machine. Particularly the OSPP.VBS file that we are going to use in this script.
 If(Test-Path -Path "C:\Program Files (x86)\Microsoft Office\Office16"){
     $O365LicenseInfo = cscript.exe //nologo "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus | Out-String
 }
+
 If(Test-Path -Path "C:\Program Files\Microsoft Office\Office16"){
     $O365LicenseInfo = cscript.exe //nologo "C:\Program Files\Microsoft Office\Office16\OSPP.VBS" /dstatus | Out-String
 }
 
-# Let's write the results back to the Powershell console.
-Write-Output "UPSTREAM: Detailed Office 365 license information:" $O365LicenseInfo
+# This If/Else section will determine if we got anything back from the $O365LicenseInfo variable above and act on that.
+If ($O365LicenseInfo -Match "Processing"){
+	# Let's write the results from above back to the Powershell console.
+	Write-Output "UPSTREAM: Detailed Office 365 license information:" $O365LicenseInfo
 
-# Now, let's check for any license issues. Anything regarding ---NOTIFICATIONS--- would indicate something is wrong with the licensing. 
-if ($O365LicenseInfo -Match "---NOTIFICATIONS---"){
-	Write-Output "UPSTREAM: Whoops. There may be potential problems with Office 365 licensing. Please investigate. Additional Windows Event Log will be written for alarm generation."
-	
-	# This If/Else section will check if the UpstreamPowerPack Event Log source exists on local machine. We use it for error logging.
-	$UpstreamPowerPacklogFileExists = [System.Diagnostics.EventLog]::SourceExists("UpstreamPowerPack");
-	If ($UpstreamPowerPacklogFileExists -Match "True"){
-		Write-Output "UPSTREAM: UpstreamPowerPack Event Log source exists."
+	# Now, let's check for any license issues. Anything regarding "---NOTIFICATIONS---" would indicate something is wrong. 
+	if ($O365LicenseInfo -Match "---NOTIFICATIONS---"){
+		Write-Output "UPSTREAM: Whoops. There may be potential problems with Office 365 licensing. Please investigate. Additional Windows Event Log will be created for your RMM to pick up."
+		
+		# This If/Else section will check if the UpstreamPowerPack Event Log Source exists on local machine. We use it to create alarms based on most RMM's Event Log parsing capabilities.
+		$UpstreamPowerPacklogFileExists = [System.Diagnostics.EventLog]::SourceExists("UpstreamPowerPack");
+		If ($UpstreamPowerPacklogFileExists -Match "True"){
+			# Great. UpstreamPowerPack Event Log Source exists on local machine. Probably created from anoter of our scripts."
+		}
+		Else{
+			# UpstreamPowerPack Event Log Source does not exist. Let's create.
+			New-EventLog -LogName System -Source UpstreamPowerPack	
+		}	
+		# This line will create the Windows Event Log for your RMM to pick up.
+		Write-EventLog -LogName System -Source UpstreamPowerPack -EventId 10 -Entrytype Information -Message "UPSTREAM: Whoops. There may be potential problems with Office 365 licensing. Please investigate. Detailed licesne information: $($O365LicenseInfo)"
 	}
 	Else{
-		# Let's create the Event Log source om the system if missing.
-		Write-Output "UPSTREAM: UpstreamPowerPack Event Log source does not exists. Creating."
-		New-EventLog -LogName System -Source UpstreamPowerPack	
-	}	
-	
-	Write-EventLog -LogName System -Source UpstreamPowerPack -EventId 10 -Entrytype Information -Message "UPSTREAM: Whoops. There may be potential problems with Office 365 licensing. Please investigate. Detailed licesne information: $($O365LicenseInfo)"
+		Write-Output "UPSTREAM: Office 365 license(s) found, activated and looking good!"	
+	}
 }
 Else{
-	Write-Output "UPSTREAM: Office 365 license(s) found, activated and looking good!"	
+	Write-Output "UPSTREAM: We can't find OSPP.VBS on this machine. Nothing to do."
 }
