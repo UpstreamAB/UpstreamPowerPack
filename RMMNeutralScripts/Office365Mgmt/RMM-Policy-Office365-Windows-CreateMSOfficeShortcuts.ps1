62af7d4a818f70f1837d4a972581a044627fd5e6
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-05-13 14:45:07 +0200

    Create RMM-Policy-Office365-Windows-CreateMSOfficeShortcuts.ps1

---
+++ RMMNeutralScripts/Office365Mgmt/RMM-Policy-Office365-Windows-CreateMSOfficeShortcuts.ps1
@@ -0,0 +1,81 @@
+# Script name: RMM-Policy-Office365-Windows-CreateMSOfficeShortcuts.ps1
+# Script type: Powershell
+# Script description: Crates desktop shortcuts to Word, Excel and PowerPoint on local machine.
+# Dependencies: Windows OS.
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack/
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# Let's look for Office 2016 on the local machine. Winword.exe 32Bit are used to validate.
+# We are creating shortcuts for Word, Excel and PowerPoint.
+If (Test-Path "C:\Program Files (x86)\Microsoft Office\Root\Office16\Winword.exe"){
+    # Let's create a shortcut for Microsoft Word.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Winword.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft Word.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()
+	
+	# Let's create a shortcut for Microsoft Excel.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Excel.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft Excel.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()
+	
+	# Let's create a shortcut for Microsoft PowerPoint.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Powerpnt.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft PowerPoint.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()	
+}
+
+# Let's look for Office 2016 on the local machine. Winword.exe 64Bit are used to validate.
+# We are creating shortcuts for Word, Excel and Powerpoint.
+If (Test-Path -Path "C:\Program\Microsoft Office\Root\Office16\Winword.exe"){
+    # Let's create a shortcut for Microsoft Word.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Winword.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft Word.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()
+	
+	# Let's create a shortcut for Microsoft Excel.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Excel.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft Excel.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()
+	
+	# Let's create a shortcut for Microsoft PowerPoint.
+	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Powerpnt.exe"
+	$ShortcutLocation = "C:\Users\public\Desktop\Microsoft PowerPoint.lnk"
+	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+	$Shortcut.TargetPath = $SourceFileLocation
+	#Save the Shortcut to the TargetPath
+	$Shortcut.Save()	
+}
