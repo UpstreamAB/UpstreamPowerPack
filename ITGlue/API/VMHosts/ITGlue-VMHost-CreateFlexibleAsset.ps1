821ed3a529082675e33f4bb62646c7fa939bb6f6
Emile Priller <emile@upstream.se>
2019-06-25 15:30:43 +0200

    Added HTML links to agent names

--- ITGlue/API/VMHosts/ITGlue-VMHost-CreateFlexibleAsset.ps1
+++ ITGlue/API/VMHosts/ITGlue-VMHost-CreateFlexibleAsset.ps1
@@ -7,257 +7,244 @@
 # --------------------------------------------------------------------------------------------------------------------------------
 
 $data = @{
-  type = "flexible_asset_types"
-  attributes = @{
-    icon = "cubes"
-    description = "This Flexible Asset is to be used to automate VM host documentation."
-    name = "VM Host"
-    enabled = $true
-  }
-  relationships = @{
-    flexible_asset_fields = @{
-      data = @(
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 1
-            name = "VM host name"
-            kind = "Text"
-            hint = "This is the unique name and identifier of this Flexible Asset. It has to match the actual name of the VM Host to be docuemented with the associated Powershell script."
-            required = $true
-            use_for_title = $true
-            expiration = $false
-            show_in_list = $true
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 2
-            name = "Documentation automation script last queried this VM host on"
-            kind = "Text"
-            hint = "Specifies the last time the VM host was queried for updated documentation."
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 3
-            name = "VM host configuration"
-            kind = "Header"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 4
-            name = "VM host related IT Glue configuration"
-            kind = "Tag"
-            tag_type = "Configurations"
-            required = $true
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 5
-            name = "Virtualization platform"
-            kind = "Select"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-            default_value = "Hyper-V
-VMware"
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 6
-            name = "CPU"
-            kind = "Number"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 7
-            name = "RAM"
-            kind = "Number"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 8
-            name = "Disk information"
-            kind = "Textbox"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 9
-            name = "Virtual switches"
-            kind = "Textbox"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 10
-            name = "VM Host supported versions"
-            kind = "Textbox"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 11
-            name = "VM guests configuration"
-            kind = "Header"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 12
-            name = "Current number of VM guests on this VM host"
-            kind = "Number"
-            hint = "Number of guests detected on this VM host based on latest execution of the ducumentation atutomation script."
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 13
-            name = "VM guest names and information"
-            kind = "Textbox"
-            hint = "VM guest names vCPUs RAM and other infromation."
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 14
-            name = "VM guest virtual disk paths"
-            kind = "Textbox"
-            hint = "VM guests and virtual disk paths discovered on this VM host."
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 15
-            name = "VM guests snapshot information"
-            kind = "Textbox"
-            hint = "All snapshots found on the host"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 16
-            name = "VM guests BIOS settings"
-            kind = "Textbox"
-            hint = "Specifies the BIOS boot settings in each each discovered guest on this VM host."
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 17
-            name = "Assigned virtual switches and IP information"
-            kind = "Textbox"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $false
-            default_value = ""
-          }
-        },
-        @{
-          type = "flexible_asset_fields"
-          attributes = @{
-            order = 18
-            name = "This automated documentation is powered by Upstream Power Pack"
-            kind = "Header"
-            required = $false
-            use_for_title = $false
-            expiration = $false
-            show_in_list = $true
-          }
-        }
-      )
+    type = "flexible_asset_types"
+    Attributes = @{
+        icon = "cubes"
+        description = "This Flexible Asset is to be used to automate VM host documentation."
+        Name = "VM Host"
+        enabled = $true
     }
-  }
+    relationships = @{
+        flexible_asset_fields = @{
+            data = @(
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 1
+                        Name = "VM host name"
+                        kind = "Text"
+                        hint = "This is the unique name and identifier of this Flexible Asset. It has to match the actual name of the VM Host to be docuemented with the associated Powershell script."
+                        required = $true
+                        use_for_title = $true
+                        expiration = $false
+                        show_in_list = $true
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 2
+                        Name = "VM host configuration"
+                        kind = "Header"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 3
+                        Name = "VM host related IT Glue configuration"
+                        kind = "Tag"
+                        tag_type = "Configurations"
+                        required = $true
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 4
+                        Name = "Virtualization platform"
+                        kind = "Select"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                        default_value = "Hyper-V
+VMware"
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 5
+                        Name = "CPU"
+                        kind = "Number"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 6
+                        Name = "RAM (GB)"
+                        kind = "Number"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 7
+                        Name = "Disk information"
+                        kind = "Textbox"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 8
+                        Name = "Virtual switches"
+                        kind = "Textbox"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 9
+                        Name = "VM guests configuration"
+                        kind = "Header"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 10
+                        Name = "Current number of VM guests on this VM host"
+                        kind = "Number"
+                        hint = "Number of guests detected on this VM host based on latest execution of the ducumentation atutomation script."
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 11
+                        Name = "VM guest names and information"
+                        kind = "Textbox"
+                        hint = "VM guest names vCPUs RAM and other infromation."
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 12
+                        Name = "VM guest virtual disk paths"
+                        kind = "Textbox"
+                        hint = "VM guests and virtual disk paths discovered on this VM host."
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 13
+                        Name = "VM guests snapshot information"
+                        kind = "Textbox"
+                        hint = "All snapshots found on the host"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 14
+                        Name = "VM guests BIOS settings"
+                        kind = "Textbox"
+                        hint = "Specifies the BIOS boot settings in each each discovered guest on this VM host."
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 15
+                        Name = "Assigned virtual switches and IP information"
+                        kind = "Textbox"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = ""
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 16
+                        Name = "Force manual sync now?"
+                        kind = "Select"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $false
+                        default_value = "Yes
+No"
+                    }
+                },
+                @{
+                    type = "flexible_asset_fields"
+                    Attributes = @{
+                        order = 17
+                        Name = "This automated documentation is powered by Upstream Power Pack"
+                        kind = "Header"
+                        required = $false
+                        use_for_title = $false
+                        expiration = $false
+                        show_in_list = $true
+                    }
+                }
+            )
+        }
+    }
 }
 
 
