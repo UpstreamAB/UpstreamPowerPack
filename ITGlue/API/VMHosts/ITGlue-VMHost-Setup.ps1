8cbc553283b8e66c0eda9ad98979d36754cec372
Emile Priller <emile@upstream.se>
2019-06-28 09:39:09 +0200

    Asks for subdomain and adds it to the task

--- ITGlue/API/VMHosts/ITGlue-VMHost-Setup.ps1
+++ ITGlue/API/VMHosts/ITGlue-VMHost-Setup.ps1
@@ -14,12 +14,13 @@ try {
 }
 
 $flexible_asset_id = Read-Host "Enter flexible asset id (unique ID for asset to update)"
+$subdomain = Read-Host "Enter the subdomain of your IT Glue domain (without eu if present)"
 
 # Create scheduled task
 # Action
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument ('-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden "{0}\ITGlue-VMHost-FeedFlexibleAssetHyperV.ps1 -flexible_asset_id {1}"' -f $PSScriptRoot, $flexible_asset_id)
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument ('-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden "{0}\ITGlue-VMHost-FeedFlexibleAssetHyperV.ps1 -flexible_asset_id {1} -subdomain {2}"' -f $PSScriptRoot, $flexible_asset_id, $subdomain)
 # Trigger
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+$trigger = New-ScheduledTaskTrigger -Daily -At (Get-Date)
 # Settings
 $settings = New-ScheduledTaskSettingsSet -WakeToRun -RestartCount 3 -RunOnlyIfNetworkAvailable -StartWhenAvailable -RestartInterval (New-TimeSpan -Minutes 3)
 # Add to task scheduler
