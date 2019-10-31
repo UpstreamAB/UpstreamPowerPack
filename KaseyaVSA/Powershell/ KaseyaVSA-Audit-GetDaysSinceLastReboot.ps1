dced16d369c70edab0b7d799bc0a4fdbd7b2c262
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-10-31 08:20:19 +0100

    Update and rename DaysSinceLastReboot.ps1 to  KaseyaVSA-Audit-GetDaysSinceLastReboot.ps1

---
+++ KaseyaVSA/Powershell/ KaseyaVSA-Audit-GetDaysSinceLastReboot.ps1
@@ -0,0 +1,18 @@
+# Script name: KaseyaVSA-Audit-GetDaysSinceLastReboot.ps1
+# Related Kaseya Agent Procedure: "Audit - Windows 10 - Get Days Since Last Reboot"
+# Script description: Get the number of days since last real system reboot, not just when the system was shut down or hibernated.
+# Upload this Powershell script to your Kaseya Agent Procedures folder "VSASharedFiles\UpstreamPowerPack\Powershell".
+# Dependencies: Existing registry values
+# Supported OS: Windows 10.
+# Script maintainer: powerpack@upstream.se
+# https://en.upstream.se/powerpack"
+
+-----------------------------------------------------------------------------------------------------------------------
+function Get-Uptime {
+   $os = Get-WmiObject win32_operatingsystem
+   $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
+   $Display = "UPSTREAM: Machine Compliance Check: Uptime In Days: " + $Uptime.Days
+   Write-Output $Display
+}
+
+Get-Uptime
