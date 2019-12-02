e7f6efd8c4aea038aa37e0f0c1102d6897838ea6
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2019-12-02 10:06:41 +0100

    Update RMM-Audit-Windows-GetWifiNames.ps1

--- RMMNeutralScripts/Audit/RMM-Audit-Windows-GetWifiNames.ps1
+++ RMMNeutralScripts/Audit/RMM-Audit-Windows-GetWifiNames.ps1
@@ -1,17 +1,2 @@
-<#	
-	===========================================================================
-		Filename:     	RMM-Audit-Windows-GetWifiNames.ps1
-		Latest update:  2019-01-19
-		Created by:   	powerpack@upstream.se
-		Organization: 	Upstream AB, https://en.upstream.se/powerpack
-	===========================================================================
-	
-	.DESCRIPTION
-		Get the history of registered Wifi names saved on local machine for audit and troubleshooting purposes.
-  	
-	.CHANGELOG
-		First release.
-#>
-
 $WifiNames = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{ $name = $_.Matches.Groups[1].Value.Trim(); $_ } | %{ (netsh wlan show profile name="$name" key=clear) } | Select-String "Key Content\W+\:(.+)$" | %{ [PSCustomObject]@{ Detected_Wifi_Names = $name } } | Format-Table -AutoSize
 Write-Output "UPSTREAM:" $WifiNames
