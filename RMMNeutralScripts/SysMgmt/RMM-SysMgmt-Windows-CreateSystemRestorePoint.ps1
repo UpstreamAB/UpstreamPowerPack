cb53b3505e9eccf5c1ce1bf1838b285c1d8ef3b9
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-06-30 16:57:57 +0200

    Delete RMM-SysMgmt-Windows-CreateSystemRestorePoint.ps1

--- RMMNeutralScripts/SysMgmt/RMM-SysMgmt-Windows-CreateSystemRestorePoint.ps1
+++
@@ -1,7 +0,0 @@
-# Script name: RMM-SysMgmt-Windows-CreateSystemRestorePoint.ps1
-# Script type: Powershell
-# Script description: Creates system restore point
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack/
-# --------------------------------------------------------------------------------------------------------------------------------
-Checkpoint-Computer -Description UpstreamPowerPackRecoveryPoint
