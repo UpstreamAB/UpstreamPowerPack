23aa35877ee2791796f7500f0546fd9db03b4bf1
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-06-30 16:56:21 +0200

    Delete RMM-Audit-Windows-GetWifiNames.ps1

--- RMMNeutralScripts/Audit/RMM-Audit-Windows-GetWifiNames.ps1
+++
@@ -1,2 +0,0 @@
-$WifiNames = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{ $name = $_.Matches.Groups[1].Value.Trim(); $_ } | %{ (netsh wlan show profile name="$name" key=clear) } | Select-String "Key Content\W+\:(.+)$" | %{ [PSCustomObject]@{ Detected_Wifi_Names = $name } } | Format-Table -AutoSize
-Write-Output "UPSTREAM:" $WifiNames
