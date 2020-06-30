ddd49ba5ae69f1179b3c18abe108e6b94c62e595
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-06-30 16:55:50 +0200

    Delete RMM-Audit-Windows-GetAffectedHPESSDDisk.ps1

--- RMMNeutralScripts/Audit/RMM-Audit-Windows-GetAffectedHPESSDDisk.ps1
+++
@@ -1,2 +0,0 @@
-$disk = (Get-CimInstance -ClassName win32_DiskDrive | Select -Expand Model).Trim(); if($disk -match 'HPE?.+SAS (2\.5 RI PLP SC SSD|2\.5 MU PLP SC SSD S2|SFF RI SC DS SSD|RI SFF SC DS SSD|12G RI SFF SC DS SSD|RI LFF SCC DS SPL SSD)') {$true} else {$false}
-Write-Output "UPSTREAM: Is the HPE SSD Disk: $disk"
