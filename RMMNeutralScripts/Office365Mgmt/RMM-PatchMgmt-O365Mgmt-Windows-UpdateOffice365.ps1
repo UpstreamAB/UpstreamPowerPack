19b12eee3960abec0db4f3800822debf32903a3b
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-07-06 13:37:45 +0200

    Update RMM-PatchMgmt-O365Mgmt-Windows-UpdateOffice365.ps1

--- RMMNeutralScripts/Office365Mgmt/RMM-PatchMgmt-O365Mgmt-Windows-UpdateOffice365.ps1
+++ RMMNeutralScripts/Office365Mgmt/RMM-PatchMgmt-O365Mgmt-Windows-UpdateOffice365.ps1
@@ -1,10 +1,10 @@
-# Script name: RMM-PatchMgmt-O365Mgmt-Windows-UpdateOffice365.ps1
-# Script type: Powershell
-# Script description: Updadates existing Office 365 installation with the latest patches.
-# Dependencies: Powershell 3.0
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack/
-# --------------------------------------------------------------------------------------------------------------------------------
+<#
+=================================================================================
+Filename:           RMM-PatchMgmt-O365Mgmt-Windows-UpdateOffice365.ps1
+Support type:       Upstream Power Pack
+Support:            Upstream AB, powerpack@upstream.se Last updated 2020-04-22
+=================================================================================
+#>
 
 $OfficeC2RClient = "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe"
 $Arguments = "/update user updatepromptuser=true forceappshutdown=true displaylevel=true"
