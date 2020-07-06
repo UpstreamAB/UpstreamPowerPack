c2f3356bc6e3ec01ac1c59927fd05edca5ce6d9b
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-07-06 13:27:25 +0200

    Update RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1

--- RMMNeutralScripts/Win10Mgmt/RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
+++ RMMNeutralScripts/Win10Mgmt/RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
@@ -1,8 +1,11 @@
-# Script name: RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
-# Script type: PowerShell
-# Script description: Disbles the seeding feature in Windows 10 to share downloaded patches with machines nearby.
-# Dependencies: Windows 10
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack 
+<#
+=================================================================================
+Filename:           RMM-PatchMgmt-Win10-DisableSeedingUpdates.ps1
+Kaseya Procedure:   Custom Field - Audit - Windows 10 - Get Azure AD Tenant And User
+Support type:       Upstream Premium Power Pack
+Support:            Upstream AB, premium@upstream.se Last updated 2020-04-22
+=================================================================================
+#>
 
-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name DODownloadMode -Value 0 -PassThru
+Write-Output "UPSTREAM: Disbles the seeding feature in Windows 10 to share downloaded patches with machines nearby."
+Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name DODownloadMode -Value 0 -Force
