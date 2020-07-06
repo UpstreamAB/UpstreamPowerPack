fa7bea9a81722ff4df9ffcfe20ead9881130762c
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-07-06 13:39:22 +0200

    Update RMM-UserExperience-Office365-Windows-CreateMSOfficeShortcuts.ps1

--- RMMNeutralScripts/Office365Mgmt/RMM-UserExperience-Office365-Windows-CreateMSOfficeShortcuts.ps1
+++ RMMNeutralScripts/Office365Mgmt/RMM-UserExperience-Office365-Windows-CreateMSOfficeShortcuts.ps1
@@ -1,13 +1,13 @@
-# Script name: RMM-UserExperience-Office365-Windows-CreateMSOfficeShortcuts.ps1
-# Script type: Powershell
-# Script description: Crates desktop shortcuts to Outlook, Word, Excel and PowerPoint on local machine.
-# Dependencies: Office 365.
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack/
-# --------------------------------------------------------------------------------------------------------------------------------
+<#
+=================================================================================
+Filename:           RMM-UserExperience-Office365-Windows-CreateMSOfficeShortcuts.ps1
+Support type:       Upstream Power Pack
+Support:            Upstream AB, powerpack@upstream.se Last updated 2020-04-22
+=================================================================================
+#>
 
 # Let's look for Office 2016 on the local machine. Winword.exe 32Bit are used to validate.
-# We are creating shortcuts for Outlook, Word, Excel and PowerPoint.
+# We are creating shortcuts for Outlook, Word and Excel.
 If (Test-Path "C:\Program Files (x86)\Microsoft Office\Root\Office16\Winword.exe"){
     # Let's create a shortcut for Microsoft Outlook.
 	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Outlook.exe"
@@ -41,21 +41,10 @@ If (Test-Path "C:\Program Files (x86)\Microsoft Office\Root\Office16\Winword.exe
 	$Shortcut.TargetPath = $SourceFileLocation
 	#Save the Shortcut to the TargetPath
 	$Shortcut.Save()
-	
-	# Let's create a shortcut for Microsoft PowerPoint.
-	$SourceFileLocation = "C:\Program Files (x86)\Microsoft Office\Root\Office16\Powerpnt.exe"
-	$ShortcutLocation = "C:\Users\public\Desktop\PowerPoint.lnk"
-	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
-	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
-	$WScriptShell = New-Object -ComObject WScript.Shell
-	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
-	$Shortcut.TargetPath = $SourceFileLocation
-	#Save the Shortcut to the TargetPath
-	$Shortcut.Save()	
 }
 
 # Let's look for Office 2016 on the local machine. Winword.exe 64Bit are used to validate.
-# We are creating shortcuts for Outlook, Word, Excel and Powerpoint.
+# We are creating shortcuts for Outlook, Word and Excel.
 If (Test-Path -Path "C:\Program\Microsoft Office\Root\Office16\Outlook.exe"){
     # Let's create a shortcut for Microsoft Word.
 	$SourceFileLocation = "C:\Program\Microsoft Office\Root\Office16\Outlook.exe"
@@ -89,15 +78,4 @@ If (Test-Path -Path "C:\Program\Microsoft Office\Root\Office16\Outlook.exe"){
 	$Shortcut.TargetPath = $SourceFileLocation
 	#Save the Shortcut to the TargetPath
 	$Shortcut.Save()
-	
-	# Let's create a shortcut for Microsoft PowerPoint.
-	$SourceFileLocation = "C:\Program\Microsoft Office\Root\Office16\Powerpnt.exe"
-	$ShortcutLocation = "C:\Users\public\Desktop\PowerPoint.lnk"
-	#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
-	#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
-	$WScriptShell = New-Object -ComObject WScript.Shell
-	$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
-	$Shortcut.TargetPath = $SourceFileLocation
-	#Save the Shortcut to the TargetPath
-	$Shortcut.Save()	
 }
