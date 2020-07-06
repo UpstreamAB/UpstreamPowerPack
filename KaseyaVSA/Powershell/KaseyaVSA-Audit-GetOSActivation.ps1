b0eb02a122895a14c40d54cae45862faddb3df3c
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-07-06 11:43:27 +0200

    Delete KaseyaVSA-Audit-GetOSActivation.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetOSActivation.ps1
+++
@@ -1,9 +0,0 @@
-# Script name: KaseyaVSA-Audit-GetOSActivation.ps1
-# Related Kaseya Agent Procedure: "Audit - Windows - Is Windows Activated"
-# Script description: Audit if the Windows OS is activated with a valid license.
-# Upload this Powershell script to your Kaseya Agent Procedures, Manged Files folder "VSASharedFiles\UpstreamPowerPack\Powershell".
-# Supported OS: Windows OS.
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack
-
-Get-CimInstance -ClassName SoftwareLicensingProduct | Where{$_.PartialProductKey -and $_.Name -Like "*Windows(R)*"} | Select -Expand LicenseStatus
