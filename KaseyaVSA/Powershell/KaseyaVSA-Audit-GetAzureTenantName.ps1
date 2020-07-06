78e619c5472d32ca009a9baf37f15ffded6e90e3
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2020-07-06 11:47:37 +0200

    Update KaseyaVSA-Audit-GetAzureTenantName.ps1

--- KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureTenantName.ps1
+++ KaseyaVSA/Powershell/KaseyaVSA-Audit-GetAzureTenantName.ps1
@@ -1,17 +1,19 @@
-# Script name: KaseyaVSA-Audit-GetAzureTenantName.ps1
-# Related Kaseya Agent Procedure: "Audit - Windows 10 - Custom Field - Azure AD Info"
-# Script description: Audit the Azure tenant name on a Windows 10 computer if present.
-# Upload this Powershell script to your Kaseya Agent Procedures folder "VSASharedFiles\UpstreamPowerPack\Powershell".
-# Supported OS: Windows 10
-# Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack
+<#
+=================================================================================
+Filename:           UPSTREAM-KaseyaVSA-Audit-Win10-GetAzureTenantName.ps1
+Kaseya Procedure:   Custom Field - Audit - Windows 10 - Get Azure AD Tenant And User
+Support type:       Upstream Premium Power Pack
+Support:            Upstream AB, powerpack@upstream.se Last updated 2020-04-22
+=================================================================================
+#>
 
-$subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo"
+Try{
+    $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/TenantInfo" -ErrorAction Stop
+    $guids = $subKey.GetSubKeyNames()
+    foreach($guid in $guids) {
+        $guidSubKey = $subKey.OpenSubKey($guid)
+        $AzureTenantName = $guidSubKey.GetValue("DisplayName")}
+        Write-Output $AzureTenantName}
 
-$guids = $subKey.GetSubKeyNames()
-foreach($guid in $guids) {
-    $guidSubKey = $subKey.OpenSubKey($guid);
-    $DisplayName = $guidSubKey.GetValue("DisplayName");
-}
-# Let's write the Azure Tenant name to the concole for Kaseya VSA to pick up as a variable.
-write-output $DisplayName
+Catch{
+    Write-Output "Not Detected"}
