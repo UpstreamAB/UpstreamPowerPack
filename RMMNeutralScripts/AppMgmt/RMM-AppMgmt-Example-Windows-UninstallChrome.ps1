8feb4295cf4272d068f286df6f58f7ebed995929
Ronny Tunfjord <38205870+eRONtu@users.noreply.github.com>
2021-03-12 08:02:21 +0100

    Update RMM-AppMgmt-Example-Windows-UninstallChrome.ps1

--- RMMNeutralScripts/AppMgmt/RMM-AppMgmt-Example-Windows-UninstallChrome.ps1
+++ RMMNeutralScripts/AppMgmt/RMM-AppMgmt-Example-Windows-UninstallChrome.ps1
@@ -1,9 +1,7 @@
 # Script name: RMM-AppMgmt-Windows-UninstallChrome.ps1
 # Script type: Powershell
 # Script description: Uninstalls Google Chrome on local machine.
-# Dependencies: Powershell 3.0
 # Script maintainer: powerpack@upstream.se
-# https://en.upstream.se/powerpack/
 # --------------------------------------------------------------------------------------------------------------------------------
 
 # What application do you want to uninstall? Use the name from the Add/Remove Control Panel.
@@ -17,8 +15,7 @@ Write-Output "UPSTREAM: Executing script to remove:" $AppName
 $AppRemove = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "$AppName" }
 
 # If the $AppRemove vairable matches the $AppName we are looking for, let's move on.
-if ($AppRemove -Match $AppName)
-{
+If ($AppRemove -Match $AppName){
 	# Let's be sure the application is isn't running before we start the uninstall.
 	Write-Output "UPSTREAM: Application found and will be stopped if running. Attempting to uninstall."
 	Get-Process | Where { $_.Name -match "$AppProcessName" } | Stop-Process -Force
@@ -26,17 +23,12 @@ if ($AppRemove -Match $AppName)
 	
 	# Let's test again if the application still is present on the local machine. We don't want that.
 	$IsAppRemovedOrNot = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -match "$AppName" }
-	if ($IsAppRemovedOrNot -Match $AppName)
-	{
-		Write-Output "UPSTREAM: Application failed to uninstall. Evaluate the Application Event Log."
-	}
-	else
-	{
-		Write-Output "UPSTREAM: Application successfully uninstalled."
-	}
+	If ($IsAppRemovedOrNot -Match $AppName){
+		Write-Output "UPSTREAM: Application failed to uninstall. Evaluate the Application Event Log."}
+	Else{
+		Write-Output "UPSTREAM: Application successfully uninstalled."}
 }
-else
-{
+
+Else{
 	# The application to be uninstalled isn't installed in the first place.
-	Write-Output "UPSTREAM: Application not found. Nothing to do."
-}
+	Write-Output "UPSTREAM: Application not found. Nothing to do."}
